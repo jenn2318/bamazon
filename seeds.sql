9779c5779a55f55c3ad841271ca93a1c9a3e05bc
Jennifer Ponder <jennp@Jennifers-MacBook-Pro.local>
2018-05-01 16:27:46 -0400

    seeds added

---
+++ seeds.sql
@@ -0,0 +1,3 @@
+
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity;
+VALUES ("bananas", "shampoo", "cookies", "chicken", "nail polish", "makeup", "books", "markers", "wine", "t-shirt"); 
