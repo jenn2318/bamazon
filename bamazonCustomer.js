30a4a19688e40eada3baf06f14864027c5359313
Jennifer Ponder <jennp@Jennifers-MacBook-Pro.local>
2018-05-05 10:12:53 -0400

    wip updated

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,7 @@
 
 let mysql = require("mysql");
 let inquirer = require("inquirer");
+let Table = require("easy-table");
 
 
 let choicesArr = [];
@@ -21,11 +22,12 @@ let connection = mysql.createConnection({
   database: "bamazon"
 });
 
-connection.connect(function(err) {
+connection.connect(function(err, res) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId);
-    purchaseStuff();
-
+    showProducts();
+    afterConnection();
+    printStuff(res);
 
 });
 
@@ -33,16 +35,38 @@ connection.connect(function(err) {
 function afterConnection() {
   connection.query("SELECT * FROM products", function(err, res) {
       if (err) throw err;
-      console.log(res);
       //connection.end();
   });
 }
 
-    //}
+
+function printStuff (res) {
+
+
+    let data = [
+        { id: 1, desc: 'banana', department_name: res.produce, price: 1.00, stock_quanity:50 },
+        { id: 2, desc: 'shampoo', department_name: res.beauty, price: 19.00, stcok_quanity:20},
+        { id: 3, desc: 'cookies', department_name: res.snacks, price: 2.00, stock_quantity:15 }
+    ]
+
+    let t = new Table
+
+    data.forEach(function (product) {
+        t.cell('Item ID', product.item_id);
+        t.cell('Product Name', product.product_name);
+        t.cell('Department Name', product.department_name);
+        t.cell('Price, USD', product.price, Table.number(2))
+        t.cell('Stock Quantity', product.stock_quantity);
+        t.newRow()
+    });
+
+    console.log(t.toString());
+}
+
 
 
 //function made for user to choose an ID of the Item they would like to purchase
-purchaseStuff = function () {
+showProducts = function () {
     connection.query('SELECT * FROM products', function (err, res) {
         let choicesArr = [];
         // Display all items
@@ -54,7 +78,7 @@ purchaseStuff = function () {
         console.log("-----");
 
 
-// inquirer to prompt the items they want to buy
+// inquirer to prompt the user of the item ID they would like to buy
 
         inquirer
             .prompt([{
@@ -86,20 +110,17 @@ purchaseStuff = function () {
             .then(function (answer) {
                 let chosenID = answer.id - 1;
                 let chosenQuantity = answer.count;
-                //after taking in amount, verify if we have enough quantity
+                //after taking in amount user wants, verify the quantity is there
                 if (chosenQuantity < res[chosenID].stock_quantity) {
-                    //If i have i should update MySQL
-                    connection.query("UPDATE products SET ? WHERE ?", [{
-                        //mysql will update quantity
-                        stock_quantity: res[chosenID].stock_quantity - chosenQuantity
-                    }, {
+                    //mysql should update stock quantity here
+                    connection.query("UPDATE products SET ? WHERE ?", [{stock_quantity: res[chosenID].stock_quantity - chosenQuantity}, {
                         item_id: res[chosenID].item_id
                     }], function (err, res) {
                         console.log(res);
                     });
                 } else {
                     console.log("Insufficient quantity for the item you wanted!");
-                    //purchaseStuff();
+
 
                 }
             });
@@ -109,10 +130,10 @@ purchaseStuff = function () {
 
 //Function to show the price, still working on this function
 //function purchaseMade() {
-    //stock_onhand = stock_left
-  //  let query = "SELECT products WHERE price=?",
+  //  stock_onhand = stock_left
+   //let query = "SELECT products WHERE=item_id=? AND SET price=?",
     //connection.query(query,function(err, res){
-      //  console.log(res);
-    //  console.log("You're total price is $" + total_price);
-    //});
-      //purchaseMade();
+      // console.log(err);
+     //console.log("You're total price is $" + total_price);
+    //}
+
